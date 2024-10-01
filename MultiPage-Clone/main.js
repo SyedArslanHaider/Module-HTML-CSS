3d5f391f5e162355a160c4e94e5c289cffd19ce4
Syed Arslan <arslanhaider231@gmail.com>
2024-10-01 11:25:42 +0200

    adjusting media quieries

--- MultiPage-Clone/main.js
+++ MultiPage-Clone/main.js
@@ -1,7 +1,14 @@
 const burgerr = document.querySelector('.navimage');
 const nav = document.querySelector('.nav-links');
+const body = document.querySelector('body');
 
 burgerr.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
     burgerr.classList.toggle('toggle');
+
+      if (nav.classList.contains('nav-active')) {
+        body.style.overflow = 'hidden';  
+    } else {
+        body.style.overflow = 'auto'; 
+    }
 });
